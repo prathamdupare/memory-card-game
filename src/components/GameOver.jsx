11ec76c6b28a9407d0213bd8b2cb46c0cff7bc26
prathamdupare <prathamdupare@proton.me>
2023-10-11 15:32:18 +0530

    Added Restart button after failing

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,11 +1,14 @@
-function GameOver(onClick) {
+function GameOver() {
+  const handleRestart = () => {
+    window.location.reload(); // Reloads the entire app
+  };
   return (
     <>
       <div className="overlay" id="gameOverOverlay">
         <div className="dialog-box">
           <h1>Game Over</h1>
           <p>You Failed!</p>
-          <button className="btn" onClick={onClick}>
+          <button className="btn" onClick={handleRestart}>
             <span className="btn__inner">
               <span className="btn__slide"></span>
               <span className="btn__content">Retry?</span>
